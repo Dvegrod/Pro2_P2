b970fa727746faaf492af519ed7f153139b848bc
Carlos Torres <>
2019-03-29 17:15:24 +0100

    Update CenterList.pas

--- CenterList.pas
+++ CenterList.pas
@@ -31,53 +31,53 @@ interface
 					fin: tPosC;
 			end;
 
-	procedure createEmptyList(var L: tListC);
+	procedure createEmptyCenterList(var L: tListC);
     (*  Objetivo: Crea una lista vacía
         Entradas: La variable donde se va a almacenar la lista
         Salidas: La lista Vacía
         Postcondición: La lista queda inicializada y no contiene elementos  *)
-	function isEmptyList(L:tListC): boolean;
+	function isEmptyCenterList(L:tListC): boolean;
     (*  Objetivo: Comprueba si la lista está vacía
         Entrada: Una lista
         Salida: Un boolean TRUE si está vacía y FALSE si no lo está *)
-	function first(L:tListC):tPosC;
+	function firstC(L:tListC):tPosC;
     (*  Objetivo: Devuelve la posición del primer elemento de la lista
         Entrada: Una lista
         Salida: La posición del primer elemento
         Precondición: La lista no está vacía    *)
-	function last(L:tListC):tPosC;
+	function lastC(L:tListC):tPosC;
     (*  Objetivo: Devuelve la posición del último elemento de la lista
         Entrada: Una Lista
         Salida: La posición del último elemento
         Precondición: La lista no está vacía    *)
-	function next(p:tPosC; L:tListC):tPosC;
+	function nextC(p:tPosC; L:tListC):tPosC;
     (*  Objetivo: Devuelve la posición en la lista del elemento siguiente al indicado
         Entrada: Una Lista
         Salida: La posición del elemento siguiente
         Precondición: La posición indicada es una posición válida*)
-	function previous(p:tPosC; L:tListC):tPosC;
+	function previousC(p:tPosC; L:tListC):tPosC;
 	(*	Objetivo: Devuelve la posición en la lista del elemento anterior al indicado
 		Entrada: Una lista
 		Salida: La posición del elemento anterior
 		Precondición: La posición indicada es una posición válida*)
-	function insertItem(d:tItemC; var L : tListC): boolean;
+	function insertItemC(d:tItemC; var L : tListC): boolean;
 	(*	Objetivo: Inserta un elemento en la lista en la posicion que le corresponde
    por orden del alfabeto (nombre del centro)
 		Entrada: Un elemento a insertar, una posición y una lista
 		Salida: La lista con el elemento insertado y un boolean TRUE si se ha insertado correctamente y un FALSE en caso contrario
 		Postcondición: Las posiciones de los elementos de la lista posteriores al insertado pueden cambiar de valor*)
-	procedure deleteAtPosition(p:tPosC; var L : tListC);
+	procedure deleteCenterAtPosition(p:tPosC; var L : tListC);
 	(*	Objetivo: Elimina de la lista el elemento que ocupa la posición indicada
 		Entrada: Una lista y una posición
 		Salida: El elemento en la posición indicada en esa lista
 		Precondición La posición indicada es una posición válida en la lista
 		Postcondición: Tanto la posición del elemento eliminado como aquellas de los elementos de la lista a continuación pueden cambiar de valor*)
-	function getItem(p:tPosC; L: tListC):tItemC;
+	function getItemC(p:tPosC; L: tListC):tItemC;
 	(*	Objetivo: Devuelve el contenido del elemento de la lista que ocupa la posición indicada
 		Entrada: Una posición y una lista
 		Salida: El elemento en esa posición de la lista
 		Precondición: La posición indicada es una posición válida en la lista*)
-	function findItem(d:tCenterName; L:tListC):tPosC;
+	function findItemC(d:tCenterName; L:tListC):tPosC;
 	(*	Objetivo: Encuentra el primer partido que coincida con el partido buscado y devuelve su posición.
 		Entradas: Un partido político y una lista
 		Salidas: Una variable de posición
@@ -86,56 +86,56 @@ interface
 	
 implementation
 
-	procedure createEmptyList(var L:tListC);
+	procedure createEmptyCenterList(var L:tListC);
 		begin
 			L.fin:= NULLC
 		end;
 		
-	function isEmptyList (L: tListC): boolean;
+	function isEmptyCenterList (L: tListC): boolean;
 		begin
-			isEmptyList := (L.fin = NULLC);
+			isEmptyCenterList := (L.fin = NULLC);
 		end;
 
-	function first (L:tListC): tPosC;
+	function firstC (L:tListC): tPosC;
 		begin
-			first:= 1;
+			firstC:= 1;
 		end;
 		
-	function last (L:tListC): tPosC;
+	function lastC (L:tListC): tPosC;
 		begin
-			last:= L.fin;
+			lastC:= L.fin;
 		end;
 		
-	function previous (p: tPosC; L: tListC): tPosC;
+	function previousC (p: tPosC; L: tListC): tPosC;
 		begin
-			previous:= p-1;
+			previousC:= p-1;
 		end;
 		
-	function next (p: tPosC; L: tListC): tPosC;
+	function nextC (p: tPosC; L: tListC): tPosC;
 		begin
 			if p=L.fin then
-				next:=NULLC
+				nextC:=NULLC
 		  else
-				next:= p+1;
+				nextC:= p+1;
 		end;
 
-  function posInSer(d : tItemC; L : tListC): tPosC;
+  function posInsert(d : tItemC; L : tListC): tPosC;
   var
     p : tPosC;
     begin
-      p := first(L);
+      p := firstC(L);
        while d.centername >= L.data[p].centername do
-         p := next(p,L);
-      posInSer := p;
+         p := nextC(p,L);
+      posInsert := p;
     end;
 	
 
-	function insertItem(d: tItemC; var L:tListC):boolean;
+	function insertItemC(d: tItemC; var L:tListC):boolean;
     var
        p : tPosC;
     begin
 			if L.fin = MAXC then (*Se controla si el array está lleno*)
-				insertItem:= FALSE
+				insertItemC:= FALSE
 			else
 				begin
            if (L.fin = 0) or (d.centername >= L.data[L.fin].centername) then
@@ -145,16 +145,16 @@ implementation
              while (p <> NULLC) and (d.centername <= L.data[p].centername) do
              begin
                 L.data[p+1] := L.data[p];
-                p := previous(p,L);
+                p := previousC(p,L);
              end;
              L.data[p+1] := d;
            end;
-           insertItem:=TRUE;
+           insertItemC:=TRUE;
            L.fin := L.fin + 1;
         end;
 		end;
 
-		procedure deleteAtPosition(p: tPosC; var L:tListC);
+		procedure deleteCenterAtPosition(p: tPosC; var L:tListC);
 			var i: tPosC;
 			begin
 				L.fin := L.fin -1;
@@ -162,23 +162,23 @@ implementation
 					L.data[i] := L.data[i+1]; 
 			end;
 			
-		function getItem (p: tPosC; L: tListC): tItemC;
+		function getItemC (p: tPosC; L: tListC): tItemC;
 			begin
-				getItem:= L.data[p];
+				getItemC:= L.data[p];
 			end;
 
-		function findItem (d: tCenterName; L: tListC): tPosC;
+		function findItemC (d: tCenterName; L: tListC): tPosC;
 			var i: tPosC;
 			begin
-				if isEmptyList(L) then (*Se controla si la lista está vacía*)
-					findItem:= NULLC
+				if isEmptyCenterList(L) then (*Se controla si la lista está vacía*)
+					findItemC:= NULLC
 				else
 					i:=1;
           while (i < L.fin) and (L.data[i].centername < d) do (*Se recorre la lista buscando el elemento y se sale del bucle cuando se lega al final o se encuentra*)
 				  	i:= i+1;
 					if d = L.data[i].centername then
-						findItem:=i (*Si se encuentra, se devuelve la posición*)
+						findItemC:=i (*Si se encuentra, se devuelve la posición*)
 					else
-						findItem := NULLC; (*Si se llega al final de la lista sin encontrarlo, se devuelve NULLC*)
+						findItemC := NULLC; (*Si se llega al final de la lista sin encontrarlo, se devuelve NULLC*)
 		end;
 end.
