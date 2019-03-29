f6d3a5ded7a0ebe022afa56b5ef431fa6e9f879e
Carlos Torres <>
2019-03-29 16:13:28 +0100

    Created new ADTs

---
+++ SharedTypes.pas
@@ -0,0 +1,14 @@
+Unit SharedTypes;
+
+interface
+
+const
+    BLANKVOTE = 'B';
+    NULLVOTE = 'N';
+
+type
+    tPartyName = string;
+    tCenterName = string;
+    tNumVotes = integer;
+
+implementation
