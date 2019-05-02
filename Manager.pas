168980d54af791d73fa0298f9db0dcef31dd838a
Daniel Sergio Vega Rodriguez <dvegrod@gmail.com>
2019-05-02 22:58:58 +0200

    Micro-Details

--- Manager.pas
+++ Manager.pas
@@ -7,10 +7,10 @@ interface
   type
     tManager = tListC;
 
-
+(* Manager hace referencia a una multilista ,es decir, una variable tipo tManager*)
   procedure createEmptyManager(var Mng: tManager);
 (*
- Objetivo: inicializa un manager vacío.
+ Objetivo: inicializa una multilista.
  Entrada: una variable tipo multilista.
  Salida: la entrada inicializada y vacía.
  *)
@@ -39,7 +39,7 @@ interface
  *)
   procedure deleteManager(var Mng: tManager);
 (*
- Objetivo: elimina todos los elementos de la multilista (manager).
+ Objetivo: elimina todos los elementos de la multilista (manager) y sus respectivas estructuras subordinadas.
  Entradas: el manager que va a ser vaciado.
  Salidas: el manager vacío.
  *)
@@ -47,6 +47,7 @@ interface
 (*
  Objetivo: muestra una serie de estadísticas de votación y participación de cada uno de los centros.
  Entradas: la multilista (manager).
+ Poscondición: si la multilista está vacía el procedimiento emitirá un mensaje de error por pantalla.
  *)
   function voteInCenter(cName: tCenterName; pName: tPartyName; var Mng: tManager):boolean;
 (*
@@ -110,7 +111,7 @@ begin
   posc := findItemC(cName,Mng);
   if (posc <> NULLC) then
   begin
-    newlist := getItemC(findItemC(cName,Mng),Mng).partylist; (* Se obtiene el puntero al primer elemento *)
+    newlist := getItemC(findItemC(cName,Mng),Mng).partylist; (* Se obtiene el puntero al primer elemento de la lista de partidos del centro *)
     with newparty do begin (* Construcción del nuevo partido *)
        partyname := pName;
        numvotes := 0;
@@ -131,7 +132,7 @@ var
    plist : tPosL;
 begin
    plist := getItemC(cpos,Mng).partylist;
-   while not isEmptyList(plist) do (*Eliminación secuancial de los elementos*)
+   while not isEmptyList(plist) do (*Eliminación secuencial de los elementos*)
       deleteAtPosition(first(plist),plist);
    updateListC(plist,cpos,Mng);(* Actualización del puntero para prevenir errores *)
 end;
@@ -159,6 +160,7 @@ begin
 
             nexposc := nextC(posc,Mng); (* Haya eliminación o no se actualiza nexposc para ser la posición siguiente de posc *)
          end;
+      {----------------------------------------------}
       posc := firstC(Mng);
       with getItemC(posc,Mng) do begin (* Ahora se revisa el primer nodo de la multilista *)
          if validvotes = 0 then begin
